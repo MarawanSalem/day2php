ae8e2c0d476f8737e4f6d0c0644ae0411d3c553f
MarawanSalem <marawan1379@gmail.com>
2024-01-18 13:12:53 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!-- Lap 2 Php By Marwan Mohamed  -->
+
+<?php
+// Task1
+echo "Marwan    <br>";
+echo "Mohamed   <br>";
+echo "Moustafa  <br>";
+echo "<hr>";
+// -----------------------------------------------------------
+// Task2
+
+foreach ($_SERVER as $key => $value) {
+    echo "<li><strong>$key:</strong> $value</li>";
+}
+// -----------------------------------------------------------
+echo "<hr>";
+// Task3
+$myArr=[12 , 45 , 10 ,25];
+
+$sum = 0;
+foreach ($myArr as $number) {
+    $sum += $number;
+}
+$count = count($myArr);
+$average = ($count > 0) ? $sum / $count : 0;
+echo "Sum: $sum<br>";
+echo "Average: $average<br>";
+rsort($myArr);
+print_r($myArr);
+// -----------------------------------------------------------
+echo "<hr>";
+// Task4
+$arr2 = [ "sara" => 31 , "john" => 41 , "walaa" => 39 , "ramy" => 40];
+
+ksort($arr2);
+echo "Ascending order based on keys: ";
+print_r($arr2);
+
+asort($arr2);
+echo "<br>Ascending order based on values: ";
+print_r($arr2);
+
+krsort($arr2);
+echo "<br>Descending order based on keys: ";
+print_r($arr2);
+
+arsort($arr2);
+echo "<br>Descending order based on values: ";
+print_r($arr2);
+
+
+?>
